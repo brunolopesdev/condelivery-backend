194aba72178786667adb858d35f28dadf95370b2
Bruno Lopes <bruno.lopes@foreverliss.com.br>
2024-06-05 21:43:20 -0300

    fix: path

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,9 @@ import express from "express";
 
 const routes = Router();
 
-const uploadsPath = path.join(__dirname, "controller", "uploads");
+const uploadsPath = path.join(__dirname, "tmp", "uploads");
+
+console.log(uploadsPath);
 routes.use("/images", express.static(uploadsPath));
 
 routes.get("/reports", getReports);
